66c3c86a1a8be13789b40f3a2045fcbcce623723
Gaurav <tiwari6665@gmail.com>
2021-10-30 21:59:51 +0530

    First commit

---
+++ frontend/src/components/TopBar.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import { AppBar, Toolbar, Typography, Link, Button } from '@material-ui/core';
+import VideocamIcon from '@material-ui/icons/Videocam';
+import GitHubIcon from '@material-ui/icons/GitHub';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+        color: theme.palette.primary.contrastText,
+        marginLeft:'1rem'
+    },
+    githubButton: {
+        marginLeft:'1rem'
+    }
+}));
+const TopBar = () => {
+    const classes = useStyles();
+    return (
+        <AppBar position="static">
+            <Toolbar>
+            <VideocamIcon fontSize="large" />
+            <Typography variant="h3" className={classes.title}>
+                Video Chat App BY - VPPCOE Students.
+            </Typography>
+
+            <Typography variant="h6" style={{color:"white", textDecoration:"none"}} >
+                @Gaurav6665
+            </Typography>
+
+            <Link 
+                href="https://github.com/Gaurav6665" 
+                className={classes.githubButton}
+                target="_blank"
+            >
+                    <Button 
+                        variant="contained" 
+                        color="primary" 
+                        fullWidth 
+                        startIcon={<GitHubIcon fontSize="small" />}
+                        className={classes.button}
+                    >
+                        View Source Code
+                    </Button>
+            </Link>
+
+            </Toolbar>
+        </AppBar>
+    )
+}
+
+export default TopBar;
